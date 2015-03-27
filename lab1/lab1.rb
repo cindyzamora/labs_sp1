acd324c7188a05eb1815cae966e13caad0abd396
Cindy Zamora <cindy@bluekite.com>
2015-03-26 23:56:24 -0600

    Lab1

---
+++ lab1/lab1.rb
@@ -0,0 +1,26 @@
+module Lab1
+  def palindromo
+
+  end
+
+  def cuenta_palabras
+
+  end
+  
+  def hanoi
+
+  end
+  
+  def sort
+
+  end
+
+  def char_count
+    
+  end
+
+  def atoi
+    
+  end
+
+end
